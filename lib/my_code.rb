133f0d4e2159e88894601d51aa8265f9b5501aae
CORNELIUS OMOWAIYE <comowaiye@gmail.com>
2020-01-24 02:49:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,11 +14,11 @@ def reduce(source_array, starting_point=nil)
     sum = starting_point 
     i = 0 
   else
-    sum = array[0]
+    sum = source_array[0]
     i = 1 
   end 
-  while i < array.length 
-  sum = yield(sum, array[i])
+  while i < source_array.length 
+  sum = yield(sum, source_array[i])
   i += 1 
   end
     sum 
